4fcf062a08bc4dbd0414b0fc058c3ad0634abe86
Borja Gomez-rey delgado <bgomez-r@c3r5s5.42madrid.com>
2021-09-13 19:44:58 +0200

    Add comments to code

---
+++ cell05/ex05/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for dir in $@
+do
+  if [ -d $dir ]
+  then
+    echo “La capeta $dir ya existe.”
+Else
+    mkdir $dir
+    if [ $? -eq 0 +
+    then
+      echo “$dir se ha creaco con éxito”
+    else
+      echo “Ups! Algo ha fallado al crear $dir”
+    fi
+  fi
+done
